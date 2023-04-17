c436168a6a34ca8cecfe637bfecfb9e44221c4ce
tottan <tottan417@outlook.jp>
2023-04-17 13:51:15 +0900

    study flask

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+@app.route('/<name>')
+def hello_world(name):
+    return f'Hello, {escape(name)}!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
